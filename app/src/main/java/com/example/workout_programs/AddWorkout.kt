50410c512e83a9b529ba20b51633cb85aa1b4c5a
Theo003 <theodorosth95@gmail.com>
2019-05-09 21:04:09 +0100

    Backup before start with services

--- app/src/main/java/com/example/workout_programs/AddWorkout.kt
+++ app/src/main/java/com/example/workout_programs/AddWorkout.kt
@@ -3,10 +3,12 @@ package com.example.workout_programs
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_add_workout.*
 import kotlinx.android.synthetic.main.activity_my__workouts.*
 
 class AddWorkout : AppCompatActivity() {
+    lateinit var myData: MyDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +20,23 @@ class AddWorkout : AppCompatActivity() {
                 finish()
             }
         })
+
+        myData = MyDatabase(ctx = this)
+
+        btnAdd.setOnClickListener {
+            //val id = etID.text.toString().toLong()
+            val title = etTitle.text.toString()
+            val subTitle = etSubTitle.text.toString()
+            val rest = etRest.text.toString().toLong()
+            val rounds = etRounds.text.toString().toLong()
+            val ex1 = etName.text.toString()
+            val rep1 = etReps.text.toString().toLong()
+            val ex2 = etName2.text.toString()
+            val rep2 = etReps2.text.toString().toLong()
+            val ex3 = etName3.text.toString()
+            val rep3 = etReps3.text.toString().toLong()
+            myData.addWorkout(title, subTitle, rest, rounds, ex1, rep1, ex2, rep2, ex3, rep3)
+            Toast.makeText(this, "You have added '$title' to the programs " , Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/example/workout_programs/MyDatabase.kt
+++ app/src/main/java/com/example/workout_programs/MyDatabase.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
+data class Program(val id: Long, val title: String, val subTitle: String, val rest: Long, val rounds: Long, val ex1: String, val rep1: Long, val ex2: String, val rep2: Long, val ex3: String, val rep3: Long)
+
 class MyDatabase(ctx: Context) : SQLiteOpenHelper(ctx,"Workouts", null, 1) {
 
     override fun onCreate(db: SQLiteDatabase) {
@@ -16,13 +18,32 @@ class MyDatabase(ctx: Context) : SQLiteOpenHelper(ctx,"Workouts", null, 1) {
         onCreate(db)
     }
 
-    fun addMusic(title: String, artist: String, year: Long) : Long{
+    fun addWorkout(title: String, subTitle: String, rest: Long, rounds: Long, ex1: String, rep1: Long, ex2: String, rep2: Long, ex3: String, rep3: Long) : Long{
         val db = writableDatabase
-        val stmt = db.compileStatement("INSERT INTO Workouts (Title, SubTitle, Rest, Rounds, ExName1, Reps, ExName2, Reps2, ExName3, Reps3 ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        val stmt = db.compileStatement("INSERT INTO Workouts (Title, SubTitle, Rest, Rounds, ExName1, Reps, ExName2, Reps2, ExName3, Reps3 ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
         stmt.bindString(1, title)
-        stmt.bindString(2, artist)
-        stmt.bindLong(3, year)
+        stmt.bindString(2, subTitle)
+        stmt.bindLong(3, rest)
+        stmt.bindLong(4, rounds)
+        stmt.bindString(5, ex1)
+        stmt.bindLong(6, rep1)
+        stmt.bindString(7, ex2)
+        stmt.bindLong(8, rep2)
+        stmt.bindString(9, ex3)
+        stmt.bindLong(10, rep3)
         val id = stmt.executeInsert()
         return id
+    }
+
+    fun viewWorkout(id: Long) : Program? {
+        val db = readableDatabase
+        val id = id
+        //val cursor = db.rawQuery("SELECT * FROM Workouts WHERE Id=? ", id)
+
+        return null
+    }
+
+    fun findWorkouts () {
+        
     }
 }
